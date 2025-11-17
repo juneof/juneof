355b93d716c385892a80b1f701d89c71c00753ee
AvinashPatel15 <avinashpatel9755@gmail.com>
2025-11-17 16:44:25 +0530

    fix: remove unused variable linting rule in HomePageSection3 component

--- src/app/components/HomePageSection3.tsx
+++ src/app/components/HomePageSection3.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react/display-name */
 import React from "react";
 import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
